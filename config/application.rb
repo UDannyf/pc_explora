fe936b13f053b869c355962ada346e04476cd13f
UDannyf <danny_u89@hotmail.com>
2020-09-23 22:29:34 -0500

    add cors

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module ImageServer
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.api_only = true
-    #config.middleware.use ActionDispatch::Flash
+    #config.api_only = true
+    config.middleware.use ActionDispatch::Flash
     #config.middleware.insert_before 0, Rack::Cors do
     #  allow do
     #    origins '*'
